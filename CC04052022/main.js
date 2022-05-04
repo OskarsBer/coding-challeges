c014d21a1f443c13f15f68157fa23b0767992e31
OskarsBer <oskars.bernhards@gmail.com>
2022-05-04 16:29:41 +0300

    added another challenge - digitize

--- CC04052022/main.js
+++ CC04052022/main.js
@@ -21,3 +21,29 @@ console.log(maps([]))
 console.log(maps([1, 2, 3]), [2, 4, 6])
 console.log(maps([4, 1, 1, 1, 4]), [8, 2, 2, 2, 8])
 console.log(maps([2, 2, 2, 2, 2, 2]), [4, 4, 4, 4, 4, 4])
+
+
+
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example:
+// 348597 => [7,9,5,8,4,3]
+// 0 => [0]
+
+
+// P: 1 digit num?
+// R: Return array of digits.
+
+function digitize(n) {
+    let numArr = n.toString().split('');
+    let result = [];
+
+    for(let i = numArr.length - 1; i >= 0; i--){
+        result.push(Number(numArr[i]));
+    }
+    return result;
+  }
+
+console.log(digitize(35231),[1,3,2,5,3])
+console.log(digitize(0),[0])
