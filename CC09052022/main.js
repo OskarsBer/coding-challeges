453cee24a9456dec407f39d045fef858199aaa1d
OskarsBer <oskars.bernhards@gmail.com>
2022-05-09 17:51:40 +0300

    added code challenge (You only need one)

--- CC09052022/main.js
+++ CC09052022/main.js
@@ -25,3 +25,30 @@ function litres(time) {
   console.log(litres(11.8), 5, 'should return 5 litres');
   console.log(litres(1787), 893, 'should return 893 litres');
   console.log(litres(0), 0, 'should return 0 litres');
+
+
+  // #2
+
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+// P: Empty arrays?
+// R: Return true if array contains given value or false if it does not.
+// P: Make function that takes in array and value. Go through array and check if it contains give value. Return true or false.
+
+
+function check(a, x) {
+    return a.includes(x, 0)
+}
+
+  console.log(check([], 2))
+  console.log(check([66, 101], 66), true);
+  console.log(check([101, 45, 75, 105, 99, 107], 107), true);
+  console.log(check(['t', 'e', 's', 't'], 'e'), true);
+  console.log(check(['what', 'a', 'great', 'kata'], 'kat'), false);
+
+
