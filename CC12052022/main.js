6a6e340c19c555f05423aa4290cdabf936fb9baa
OskarsBer <oskars.bernhards@gmail.com>
2022-05-12 13:28:50 +0300

    added coding challenge and solution (conv years to centuries)

--- CC12052022/main.js
+++ CC12052022/main.js
@@ -21,3 +21,43 @@ var stringToNumber = function(str){
   console.log(stringToNumber("605"), 605)
   console.log(stringToNumber("1405"),1405)
   console.log(stringToNumber("-7"),  -7)
+
+
+  // #2
+
+
+//   Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+// P: Take year and convert it to century.
+// P: Make function that takes in num (year). Using formula convert given number to century. Return result.
+
+
+function century(year) {
+    let result;
+
+    if(year <= 100){
+        result = 1;
+    }else if(year % 100 === 0){
+        result = year / 100;
+    }else{
+        result = year / 100 + 1;
+    }
+    return Math.floor(result); // Math.floor instead of Math.round because of large nums
+}
+
+
+  console.log(century(1705), 18, 'Testing for year 1705');
+  console.log(century(1900), 19, 'Testing for year 1900');
+  console.log(century(1601), 17, 'Testing for year 1601');
+  console.log(century(2000), 20, 'Testing for year 2000');
+  console.log(century(89), 1, 'Testing for year 89');
