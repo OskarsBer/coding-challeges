68db44af0612ccfedcd61b72ddb8ed04dd903389
OskarsBer <oskars.bernhards@gmail.com>
2022-05-12 19:23:59 +0300

    added coding challenge (basic mathematical op)

--- CC12052022/main.js
+++ CC12052022/main.js
@@ -68,3 +68,41 @@ function century(year) {
   console.log(century(2000), 20, 'Testing for year 2000');
   console.log(century(89), 1, 'Testing for year 89');
 
+
+  // #3
+
+
+//   Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+// P: Make function take takes in 3 parameters. According to first parameter calculate and return result.
+
+
+function basicOp(operation, value1, value2) {
+    let result;
+
+    if(operation === '+'){
+        result = value1 + value2;
+    }else if(operation === '-'){
+        result = value1 - value2;
+    }else if(operation === '*'){
+        result = value1 * value2;
+    }else if(operation === '/'){
+        result = value1 / value2;
+    }
+    return result;
+}
+
+
+console.log(basicOp('+', 4, 7), 11);
+console.log(basicOp('-', 15, 18), -3);
+console.log(basicOp('*', 5, 5), 25);
+console.log(basicOp('/', 49, 7), 7);
