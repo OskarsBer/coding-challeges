6b53d9691aacc14dca37c6e6644bec7070c90012
OskarsBer <oskars.bernhards@gmail.com>
2022-05-13 10:07:55 +0300

    added coding challenge and solution (clock)

--- CC13052022/main.js
+++ CC13052022/main.js
@@ -26,3 +26,36 @@ function doubleInteger(i) {
 console.log(doubleInteger(2), 4);
 console.log(doubleInteger(3), 6);
 console.log(doubleInteger(4), 8);
+
+
+// #3
+
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+// P: Create function that takes in 3 parameters. Calculates time passed since midnight. Returns it in milliseconds.
+
+
+function past(h, m, s){
+    return (h*3600000) + (m*60000) + (s*1000);
+}
+
+
+console.log(past(0,1,1),61000)
+console.log(past(1,1,1),3661000)
+console.log(past(0,0,0),0)
+console.log(past(1,0,1),3601000)
+console.log(past(1,0,0),3600000)
