0e4a58035ffc0ca69caa83dbdf5bc1236b6285e8
OskarsBer <oskars.bernhards@gmail.com>
2022-05-13 09:55:01 +0300

    added coding challenge and solution (double integer)

--- CC13052022/main.js
+++ CC13052022/main.js
@@ -11,3 +11,18 @@ function booleanToString(b){
 
 console.log(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
 console.log(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
+
+
+// #2
+
+// You need to double the integer and return it.
+
+
+function doubleInteger(i) {
+    return i * 2;
+}
+
+
+console.log(doubleInteger(2), 4);
+console.log(doubleInteger(3), 6);
+console.log(doubleInteger(4), 8);
