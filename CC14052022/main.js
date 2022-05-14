53d51ab2ee4c6980a31306733aaa49274a5f863a
OskarsBer <oskars.bernhards@gmail.com>
2022-05-14 17:43:31 +0300

    Coding challeges and solutions for May 14 (Invert values)

---
+++ CC14052022/main.js
@@ -0,0 +1,20 @@
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+
+// P: Take in given array. Return inverted values.
+
+
+function invert(array) {
+    return array.map(elt => elt * (-1));
+}
+
+
+console.log(invert([1,2,3,4,5]), [-1,-2,-3,-4,-5]);
+console.log(invert([1,-2,3,-4,5]), [-1,2,-3,4,-5]);
+console.log(invert([]), []);
+console.log(invert([0]), [-0]);
