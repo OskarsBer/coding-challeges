95b605ca987561ed17ed638803ec4702e8250f86
OskarsBer <oskars.bernhards@gmail.com>
2022-05-16 09:07:44 +0300

    added coding challenge (paperwork)

--- CC16052022/main.js
+++ CC16052022/main.js
@@ -1,9 +1,35 @@
 // Write a function which converts the input string to uppercase.
 
-
 function makeUpperCase(str) {
-    return str.toUpperCase()
+  return str.toUpperCase();
+}
+
+console.log(makeUpperCase("hello"), "HELLO");
+
+// #2
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+// Waiting for translations and Feedback! Thanks!
+
+// P: Make function that take in two parameters. Calculate and return result. Check for less than zero values.
+
+
+function paperwork(n, m) {
+  if(n < 0 || m < 0){
+    return 0;
+  }
+    return n * m;
 }
 
 
-console.log(makeUpperCase("hello"), "HELLO");
+console.log(paperwork(5,5), 25, 'Failed at paperwork(5, 5)');
+console.log(paperwork(5,-5), 0, 'Failed at paperwork(5, -5)');
+console.log(paperwork(-5,-5), 0, 'Failed at paperwork(-5, -5)');
+console.log(paperwork(-5,5), 0, 'Failed at paperwork(-5, 5)');
+console.log(paperwork(5,0), 0, 'Failed at paperwork(5, 0)');
