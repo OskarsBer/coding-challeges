997273936e5ce17276fb2a7d9764fd6ac69efeb2
OskarsBer <oskars.bernhards@gmail.com>
2022-05-17 21:48:26 +0300

    Coding challenges and solutions for May 17 (avg)

---
+++ CC17052022/main.js
@@ -0,0 +1,17 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+
+function find_average(array) {
+    if(array.length <= 0){
+        return 0;
+    }
+        return array.reduce((acc, cv) => acc + cv) / array.length;
+}
+
+
+console.log(find_average([1,1,1]), 1);
+console.log(find_average([1,2,3]), 2);
+console.log(find_average([1,2,3,4]), 2.5);
+console.log(find_average([]), 0);
