a2ec2d4dcc1ef93e3aaa08416443140ff8ae6523
OskarsBer <oskars.bernhards@gmail.com>
2022-05-18 13:18:52 +0300

    Coding challenges and solutions for May 18 (bmi)

---
+++ CC18052022/main.js
@@ -0,0 +1,29 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+
+// P: Write function that takes in persons weight and height as arguments. Then calculates person's BMI according to given formula. Returns BMI string.
+
+
+function bmi(weight, height) {
+    const bodyMassIndex = weight / (height**2);
+    if(bodyMassIndex <= 18.5){
+        return "Underweight";
+    }else if(bodyMassIndex <= 25.0){
+        return "Normal";
+    }else if(bodyMassIndex <= 30.0){
+        return "Overweight"
+    }else{
+    return "Obese";
+    }
+}
+
+
+console.log(bmi(80, 1.80), "Normal")
