e380c4c0d3f843c98481f07521ab6acfb674a9c1
OskarsBer <oskars.bernhards@gmail.com>
2022-05-29 22:30:55 +0300

    added coding challenge (conv a str to arr)

--- CC29052022/main.js
+++ CC29052022/main.js
@@ -3,3 +3,20 @@
 const multiply = (num1, num2) => num1*num2;
 
 console.log(multiply(2, 3), 6);
+
+
+// CC2
+
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input -> Output):
+// * "Robin Singh" ==> ["Robin", "Singh"]
+
+// * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+const stringToArray = string => string.split(' ');
+
+
+console.log(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+console.log(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
