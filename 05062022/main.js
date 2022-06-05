02b8f4b973e7a85521508d28d78f09c4a938e753
OskarsBer <oskars.bernhards@gmail.com>
2022-06-05 16:03:05 +0300

    added coding challenge (rps)

--- 05062022/main.js
+++ 05062022/main.js
@@ -47,3 +47,42 @@ const areaOrPerimeter = (l, w) => l === w ? l**2 : 2*(l+w);
 
 console.log(areaOrPerimeter(4 , 4) , 16);
 console.log(areaOrPerimeter(6 , 10) , 32);
+
+
+// CC3
+
+
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+
+const rps = (p1, p2) => {
+    if(p1 === 'scissors' && p2 === 'paper') {
+        return `Player 1 won!`;
+    }else if(p1 === 'scissors' && p2 === 'rock') {
+        return `Player 2 won!`;
+    }else if(p1 === 'paper' && p2 === 'rock') {
+        return `Player 1 won!`;
+    }else if(p1 === 'paper' && p2 === 'scissors') {
+        return `Player 2 won!`;
+    }else if(p1 === 'rock' && p2 === 'scissors') {
+        return `Player 1 won!`;
+    }else if(p1 === 'rock' && p2 === 'paper') {
+        return `Player 2 won!`;
+    }else if(p1 === p2){
+        return `Draw!`
+    }
+};
+
+
+console.log(rps('rock', 'scissors'));
+console.log(rps('scissors', 'paper'));
+console.log(rps('paper', 'rock'));
+
+
