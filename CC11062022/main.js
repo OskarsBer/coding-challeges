330279cdcc6b65873680397507957f62b7a0fb28
OskarsBer <oskars.bernhards@gmail.com>
2022-06-11 19:17:06 +0300

    added coding challegne (goals)

--- CC11062022/main.js
+++ CC11062022/main.js
@@ -74,4 +74,32 @@ console.log(doubleChar("1337"), "11333377");
 console.log(doubleChar("illuminati"), "iilllluummiinnaattii");
 console.log(doubleChar("123456"), "112233445566");
 console.log(doubleChar("%^&*("), "%%^^&&**((");
+
+
+// CC4
+
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+
+// function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+//     // code goes here
+// }
+
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) =>
+    laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+
+console.log(goals(0,0,0), 0)
+console.log(goals(43, 10, 5), 58)
   
