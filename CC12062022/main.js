522b9e5ffc483f9bd9c8779572f92fac59aede01
OskarsBer <oskars.bernhards@gmail.com>
2022-06-12 19:31:28 +0300

    added coding challenge (removing elements)

--- CC12062022/main.js
+++ CC12062022/main.js
@@ -81,4 +81,29 @@ console.log(switchItUp(3),"Three");
 console.log(switchItUp(5),"Five");
 
 
+// CC4
+
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+
+function removeEveryOther(arr){
+    let result = [];
+    for(let i = 0; i < arr.length; i+=2) {
+        result.push(arr[i]);
+    }
+    return result;
+}
+
+
+console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again']);
+console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9]);
+console.log(removeEveryOther([[1, 2]]), [[1, 2]]);
+console.log(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]);
+
 
