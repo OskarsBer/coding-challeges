fecd0e29d630203b1e401f31a744af14712e6284
OskarsBer <oskars.bernhards@gmail.com>
2022-06-12 19:11:48 +0300

    added coding challenge (goals)

--- CC12062022/main.js
+++ CC12062022/main.js
@@ -28,3 +28,12 @@ const enough = (cap, on, wait) => cap - on - wait < 0 ? Math.abs(cap - on - wait
 console.log(enough(10, 5, 5), 0);
 console.log(enough(100, 60, 50), 10);
 console.log(enough(20, 5, 5), 0);
+
+
+// CC2
+
+var laLigaGoals = 43;
+var championsLeagueGoals = 10;
+var copaDelReyGoals = 5;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
