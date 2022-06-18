ad9554352053a2bb35775e93070e9eefd1f45f7c
OskarsBer <oskars.bernhards@gmail.com>
2022-06-18 23:22:41 +0300

    refactored coding challenge (petals)

--- CC18062022/main.js
+++ CC18062022/main.js
@@ -11,21 +11,23 @@
 // Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
 
 
-function howMuchILoveYou(nbPetals) {
-    if(nbPetals % 6 === 1) {
-        return "I love you";
-    }else if(nbPetals % 6 === 2) {
-        return "a little";
-    }else if(nbPetals % 6 === 3) {
-        return "a lot";
-    }else if(nbPetals % 6 === 4) {
-        return "passionately"
-    }else if(nbPetals % 6 === 5) {
-        return "madly"
-    }else{
-        return "not at all"
-    }
-}
+// function howMuchILoveYou(nbPetals) {
+//     if(nbPetals % 6 === 1) {
+//         return "I love you";
+//     }else if(nbPetals % 6 === 2) {
+//         return "a little";
+//     }else if(nbPetals % 6 === 3) {
+//         return "a lot";
+//     }else if(nbPetals % 6 === 4) {
+//         return "passionately"
+//     }else if(nbPetals % 6 === 5) {
+//         return "madly"
+//     }else{
+//         return "not at all"
+//     }
+// }
+
+const howMuchILoveYou= nbPetals => ['not at all','I love you','a little','a lot','passionately','madly'][nbPetals % 6]
 
 
 console.log(howMuchILoveYou(7),"I love you")
