aa2f39f346ed7ce9d781a14e98b0d30ba91a4030
OskarsBer <oskars.bernhards@gmail.com>
2022-06-20 23:18:32 +0300

    Coding challenges and solutions for June 20 (correct mistakes of the charracter recognition software)

---
+++ 20062022/main.js
@@ -0,0 +1,33 @@
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+
+function correct(string) {
+    let newStr = string.split('');
+    for(let i = 0; i < newStr.length; i++) {
+        if(newStr[i] === '5') {
+            newStr[i] = 'S';
+        }else if(newStr[i] === '0') {
+            newStr[i] = 'O';
+        }else if(newStr[i] === '1') {
+            newStr[i] = 'I';
+        }
+    }
+    return newStr.join('');
+}
+	
+
+
+console.log(correct("L0ND0N"),"LONDON");
+console.log(correct("DUBL1N"),"DUBLIN");
+console.log(correct("51NGAP0RE"),"SINGAPORE");
+console.log(correct("BUDAPE5T"),"BUDAPEST");
+console.log(correct("PAR15"),"PARIS");
