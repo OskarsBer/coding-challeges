74f9f487effae0fd2cd7e61c21a7bee936477a94
OskarsBer <oskars.bernhards@gmail.com>
2022-06-21 22:15:13 +0300

    added coding challenge (dog and cat years)

--- CC21062022/main.js
+++ CC21062022/main.js
@@ -130,3 +130,57 @@ console.log(move(0, 4), 8);
 console.log(move(3, 6), 15);
 console.log(move(2, 5), 12);
 
+
+// CC5
+
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+// References
+
+// http://www.catster.com/cats-101/calculate-cat-age-in-cat-years
+// http://www.slate.com/articles/news_and_politics/explainer/2009/05/a_dogs_life.html
+
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let catYears;
+    let dogYears;
+    
+    if(humanYears === 1) {
+        catYears = 15;
+    }else if(humanYears === 2) {
+        catYears = 24;
+    }else if(humanYears > 2) {
+        catYears = 24 + (humanYears - 2) * 4;
+    }
+
+    if(humanYears === 1) {
+        dogYears = 15;
+    }else if(humanYears === 2) {
+        dogYears = 24;
+    }else if(humanYears > 2) {
+        dogYears = 24 + (humanYears - 2) * 5;
+    }
+
+    return [humanYears, catYears, dogYears];
+}
+  
+
+console.log(humanYearsCatYearsDogYears(1), [1,15,15]);
+console.log(humanYearsCatYearsDogYears(2), [2,24,24]);
+console.log(humanYearsCatYearsDogYears(10), [10,56,64]);
