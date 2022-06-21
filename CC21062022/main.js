b9fd120847a88055469acc62b888bf9fe11074fd
OskarsBer <oskars.bernhards@gmail.com>
2022-06-21 21:32:35 +0300

    added coding challenge (sumStr)

--- CC21062022/main.js
+++ CC21062022/main.js
@@ -83,4 +83,28 @@ console.log(between(-2, 2), [-2, -1, 0, 1, 2]);
 
 // CC3
 
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+// Notes:
+
+// If either input is an empty string, consider it as zero.
+
+// Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+
+// function sumStr(a,b) {
+  
+// }
+
+const sumStr = (a, b) => String(+a + +b);
+
+console.log(sumStr("4","5"), "9");
+console.log(sumStr("34","5"), "39");
 
