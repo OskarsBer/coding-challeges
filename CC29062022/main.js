c9c9f823393d2a82d32554b9fe55e1de3555349a
OskarsBer <oskars.bernhards@gmail.com>
2022-06-29 13:52:36 +0300

    added coding challenge (geese filtered)

--- CC29062022/main.js
+++ CC29062022/main.js
@@ -47,3 +47,34 @@ console.log(isPalindrome("Bob"), true);
 console.log(isPalindrome("Madam"), true);
 console.log(isPalindrome("AbBa"), true);
 console.log(isPalindrome(""), true);
+
+
+// CC3
+
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+// The geese are any strings in the following array, which is pre-populated in your solution:
+
+//   ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+// For example, if this array were passed as an argument:
+
+//  ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+// Your function would return the following array:
+
+// ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+// The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+
+    for(let i = 0; i < geese.length; i++) {
+        birds = birds.filter(elt => elt != geese[i])        
+        }
+    return birds    
+};
+
+
+console.log(gooseFilter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]),["Mallard", "Hook Bill", "Crested", "Blue Swedish"]);
+console.log(gooseFilter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]),["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]);
+console.log(gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]),[]);
