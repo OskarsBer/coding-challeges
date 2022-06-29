ccbbe3bcab78d57eadb9929c4699f8a70bc52e57
OskarsBer <oskars.bernhards@gmail.com>
2022-06-29 13:08:24 +0300

    added coding challenge (palindrome)

--- CC29062022/main.js
+++ CC29062022/main.js
@@ -28,3 +28,22 @@ console.log(well(['good', 'bad', 'bad', 'bad', 'bad']), 'Publish!');
 console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']), 'I smell a series!');
 
 
+// CC2
+
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+
+// function isPalindrome(x) {
+//     // your code here
+// }
+
+const isPalindrome = x => x.toLowerCase() === x.split("").reverse().join("").toLowerCase();
+
+console.log(isPalindrome("a"), true);
+console.log(isPalindrome("aba"), true);
+console.log(isPalindrome("Abba"), true);
+console.log(isPalindrome("hello"), false);
+console.log(isPalindrome("Bob"), true);
+console.log(isPalindrome("Madam"), true);
+console.log(isPalindrome("AbBa"), true);
+console.log(isPalindrome(""), true);
