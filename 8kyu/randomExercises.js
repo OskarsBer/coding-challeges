5f0c6577130812589a9a9359286a68fa5afcc7c9
OskarsBer <oskars.bernhards@gmail.com>
2022-07-07 16:10:10 +0300

    CC07072022 exercise 1

---
+++ 8kyu/randomExercises.js
@@ -0,0 +1,30 @@
+//1 Get the extension of a file name.
+
+
+function getFileExtension(file) {
+    let result = file.split('.');
+    return result[1];
+}
+
+console.log(getFileExtension('script.js'))
+
+
+//2 Write a Js program to create a new string without the first and last character of a given string.
+
+//3 Write a Js program to reverse a given string.
+
+//4 Write a Js program to convert the lattrs of a given string in alphabetical order.
+
+//5 whrite a js program to check two given nummbers and 
+//return true if one of the number is 50 or if their sum is 50.
+
+// 6 whrite a js program to complite the sum of
+// three elements of a given array of integers of length 3.
+
+//7 whrite a js program to compute the sum of the two given integers. 
+//If the two values are same, then returns triple their sum.
+
+//8 Write a js program to create a newqrry taking the first and last element
+// from a given arry of integers and length must be greater or equal to 1.
+
+//9 Write a js program to find the number of elements which presents in both of the given arrays.
