14213c3a09e39bb46a041d758e91d208c25c1164
Oskars Bernhards <oskars.bernhards@gmail.com>
2022-07-21 19:13:48 +0300

    CC21072022 - XOR

---
+++ 8kyu/xor.js
@@ -0,0 +1,15 @@
+// function xor(a, b) {
+//   if(a != b) {
+//     return true;
+//   }else{
+//     return false;
+//   }
+// }
+
+const xor = (a, b) => a != b;
+
+
+console.log(xor(false, false));
+console.log(xor(true, false));
+console.log(xor(false, true));
+console.log(xor(true, true));
