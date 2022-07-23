fee39f942b55c343611d8bfc8a985910b2ffebe6
Oskars Bernhards <oskars.bernhards@gmail.com>
2022-07-23 19:05:25 +0300

    CC23072022 - ascii

---
+++ 8kyu/ascii.js
@@ -0,0 +1,19 @@
+// take num, return ascii char
+
+// function getChar(char){
+//   return String.fromCharCode(char);
+// }
+
+const getChar = char => String.fromCharCode(char);
+
+console.log(getChar(55),'7')
+console.log(getChar(56),'8')
+console.log(getChar(57),'9')
+console.log(getChar(58),':')
+console.log(getChar(59),';')
+console.log(getChar(60),'<')
+console.log(getChar(61),'=')
+console.log(getChar(62),'>')
+console.log(getChar(63),'?')
+console.log(getChar(64),'@')
+console.log(getChar(65),'A')
