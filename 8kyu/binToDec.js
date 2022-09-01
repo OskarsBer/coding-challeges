453ac57ad9a4b0765e999caef076f622e2aad151
Oskars Bernhards <oskars.bernhards@gmail.com>
2022-09-01 23:07:33 +0300

    CC01092022 - bin to dec

---
+++ 8kyu/binToDec.js
@@ -0,0 +1,10 @@
+// Convert binary num to decimal
+
+// function binToDec(bin) {
+//   return parseInt(bin, 2);
+// }
+
+const binToDec = bin => parseInt(bin, 2);
+
+console.log(binToDec(["1001001"])) // 73
+console.log(binToDec(["1"])) // 1
