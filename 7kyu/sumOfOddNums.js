c15740db94ef5f85012d622e1d6bc42a17f0ac63
Oskars Bernhards <oskars.bernhards@gmail.com>
2022-09-15 17:22:24 +0300

    CC15092022 - sum of odd nums

---
+++ 7kyu/sumOfOddNums.js
@@ -0,0 +1,9 @@
+// Given triangle of consecutive odd nums calculate sum of nums in nth row.
+// Return sum of odd nums in given row.
+
+function rowSumOddNumbers(n) {
+  return n**3;
+}
+
+console.log(rowSumOddNumbers(2))// 8
+console.log(rowSumOddNumbers(42))// 74088
