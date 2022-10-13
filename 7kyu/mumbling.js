cd852bc8113e73e11271dc75a0b089d5d29c2093
Oskars Bernhards <oskars.bernhards@gmail.com>
2022-10-13 14:48:46 +0300

    CC13102022 - mumbling string

---
+++ 7kyu/mumbling.js
@@ -0,0 +1,18 @@
+// Make mumbling function - takes in str - returns modified str.
+
+function accum(s) {
+	let newStr = s.slice().split('');
+  let result = [];
+
+  for(let i = 0; i < newStr.length; i++) {
+    let temp = newStr[i].repeat(i+1);
+    result.push(temp[0].toUpperCase() + temp.slice(1).toLowerCase());
+    }
+  return result.join('-');
+}
+
+console.log(accum("ZpglnRxqenU"))// "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+console.log(accum("NyffsGeyylB"))// "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+console.log(accum("MjtkuBovqrU"))// "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+console.log(accum("EvidjUnokmM"))// "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+console.log(accum("HbideVbxncC"))// "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
