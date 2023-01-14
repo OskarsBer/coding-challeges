84e5c5fd4d3a5830377a244ca21aa72d292af9cf
Oskars Bernhards <oskars.bernhards@gmail.com>
2023-01-14 23:37:06 +0200

    CC14012023 - intro practice

---
+++ 8kyu/cEasy.cs
@@ -0,0 +1,47 @@
+// Random 8 kyu katas to start solving Csharp problems
+
+// Fix this method
+public class CustomMath {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
+
+CustomMath(2, 4); // 8
+
+// Make func that takes in int and return "Even" or "Odd" accordingly
+
+using System;
+
+namespace Solution
+{
+  public class SolutionClass
+  {
+    public static string EvenOrOdd(int number)
+    {
+      return number % 2 == 0 ? "Even" : "Odd";
+    }
+  }
+}
+
+// Make func that converts int/float to opposite 
+
+using System;
+
+public class Kata
+{
+  public  static int Opposite(int number) => number * -1;
+}
+
+// Convert int to string
+
+using System;
+
+public class Kata
+{
+  public static string NumberToString(int num)=> num.ToString();
+}
+
+
+
+
