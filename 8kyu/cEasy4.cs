0a2285f43779e08134b15ff6a0dadf279a5d0f1a
Oskars Bernhards <oskars.bernhards@gmail.com>
2023-01-17 23:26:35 +0200

    CC17012023 - intro practice 3-2

--- 8kyu/cEasy4.cs
+++ 8kyu/cEasy4.cs
@@ -4,3 +4,8 @@ using System.Linq;
 static string RepeatStr(int n, string s) => String.Concat(Enumerable.Repeat(s, n));
 
 Console.WriteLine(RepeatStr(5, "abc"));
+
+// Remove first and last character from given string
+static string Remove_char(string s) => s.Substring(1, s.Length - 2);
+
+Console.WriteLine(Remove_char("12345"));
