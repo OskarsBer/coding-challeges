7df67aaedeb3230723d4296ce2ac9102fd1dd201
Oskars Bernhards <oskars.bernhards@gmail.com>
2023-02-01 23:40:05 +0200

    CC01022023 - cuboid

---
+++ 8kyu/cuboid.cs
@@ -0,0 +1,5 @@
+// Calculate volume of cuboid
+
+static double GetVolumeOfCuboid(double length, double width, double height) => length * width * height;
+
+Console.WriteLine(GetVolumeOfCuboid(2, 5, 6)); // 60
