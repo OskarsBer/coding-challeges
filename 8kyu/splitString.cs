35987ff93fb46ea2a8efc1e8b79e910b154300dd
Oskars Bernhards <oskars.bernhards@gmail.com>
2023-02-12 22:54:20 +0200

    CC12022023 - split string

---
+++ 8kyu/splitString.cs
@@ -0,0 +1,4 @@
+// Make function to split string and convert it to array of words
+static string[] StringToArray(string str) => str.Split();
+
+Console.WriteLine(StringToArray("Hello, nice to meet you")); // ["Hello," "nice" "to" "meet" "you"]
