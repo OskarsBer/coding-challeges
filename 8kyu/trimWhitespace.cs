a65d681a19b1d7d474c6e25928fe0cee00ebe4da
Oskars Bernhards <oskars.bernhards@gmail.com>
2023-03-16 22:59:11 +0200

    CC16032023 - Remove white space

---
+++ 8kyu/trimWhitespace.cs
@@ -0,0 +1,4 @@
+// Remove white spaces in given string
+static string NoSpace(string input) => input.Replace(" ", "");
+
+Console.WriteLine(NoSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd")); // "88Bifk8hB8BB8BBBB888chl8BhBfd"
